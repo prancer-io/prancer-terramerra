e0498d2072bd05884da8bc60983b8a5578f1b9b6
maxo <maxo.webmaster@gmail.com>
2020-09-28 00:42:16 +0200

    vnet peering terraform code added

---
+++ azure/modules/network-peering/vars.tf
@@ -0,0 +1,51 @@
+variable "peering_name" {
+  description = "name"
+}
+variable "vnet_name1" {
+  description = "name"
+}
+
+variable "vnet_name2" {
+  description = "name"
+}
+
+variable "vnet_rg1" {
+  description = "name of the resource group"
+}
+
+variable "vnet_rg2" {
+  description = "name of the resource group"
+}
+
+variable "vnet1_id" {
+  description = "vnet1_id"
+}
+
+variable "vnet2_id" {
+  description = "vnet2_id"
+}
+
+variable "allow_virtual_network_access1" {
+  description = "allow_virtual_network_access"
+}
+
+variable "allow_forwarded_traffic1" {
+  description = "allow_forwarded_traffic"
+}
+
+variable "allow_gateway_transit1" {
+  description = "allow_gateway_transit"
+}
+
+
+variable "allow_virtual_network_access2" {
+  description = "allow_virtual_network_access"
+}
+
+variable "allow_forwarded_traffic2" {
+  description = "allow_forwarded_traffic"
+}
+
+variable "allow_gateway_transit2" {
+  description = "allow_gateway_transit"
+}
---
+++ azure/vnetpeerings/terraform.tfvars
@@ -0,0 +1,21 @@
+location              = "eastus2"
+resource_group        = "prancer-test-rg"
+
+peer1_vnet            = "peer1"
+peer2_vnet            = "peer2"
+
+peer1_address_space         = "10.154.0.0/16"
+peer1_dns_server            = "10.154.0.1"
+peer2_address_space         = "10.254.0.0/16"
+peer2_dns_server            = "10.254.0.1"
+
+peering_name                  = "prancer-vnet-peering"
+allow_virtual_network_access1 = true
+allow_forwarded_traffic1      = true
+allow_gateway_transit1        = false
+allow_virtual_network_access2 = true
+allow_forwarded_traffic2      = true
+allow_gateway_transit2        = false
+
+tags                  = {}
+
