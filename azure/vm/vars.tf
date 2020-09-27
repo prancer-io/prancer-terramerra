7115ba9228f7ce191f9152a3fecf2acbe95edb5b
maxo <maxo.webmaster@gmail.com>
2020-09-28 01:29:30 +0200

    vm windows/linux terraform code added

---
+++ azure/vm/vars.tf
@@ -0,0 +1,20 @@
+variable "location" {}
+variable "vnet_name" {}
+variable "resource_group" {}
+variable "address_space" {}
+variable "dns_server" {}
+
+variable "subnet_name" {}
+variable "address_prefixes" {}
+
+variable "vm_lin_os" {}
+variable "vm_lin_size" {}
+variable "vm_lin_public_dns" {}
+
+variable "vm_win_hostname" {}
+variable "vm_win_password" {}
+variable "vm_win_os" {}
+variable "vm_win_size" {}
+variable "vm_win_public_dns" {}
+
+variable "tags" {}
