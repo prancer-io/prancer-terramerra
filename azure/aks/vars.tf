879450bdde5315512bc4276d5d545cbf4d7fc493
maxo <maxo.webmaster@gmail.com>
2020-12-17 22:46:53 +0100

    broken aks completed

--- azure/aks/vars.tf
+++ azure/aks/vars.tf
@@ -2,13 +2,16 @@ variable "aks_name" {}
 variable "location" {}
 variable "rg_name" {}
 variable "dns_prefix" {}
+variable "network_plugin" {}
 variable "network_policy" {}
 variable "aks_node_pool_name" {}
 variable "aks_node_pool_count" {}
 variable "aks_vm_size" {}
+variable "rbac_enabled" {}
 variable "http_application_routing" {}
 variable "aks_identity_type" {}
 variable "oms_agent" {}
+variable "log_analytics_workspace_id" {}
 variable "tags" {
   type = map
 }
--- azure/modules/managedClusters/main.tf
+++ azure/modules/managedClusters/main.tf
@@ -3,7 +3,11 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = var.location
   resource_group_name = var.rg_name
   dns_prefix          = var.dns_prefix
-  network_policy      = var.network_policy
+
+  network_profile {
+    network_plugin = var.network_plugin
+    network_policy = var.network_policy
+  }
 
   default_node_pool {
     name       = var.aks_node_pool_name
@@ -11,16 +15,23 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vm_size    = var.aks_vm_size
   }
 
-  http_application_routing {
-    enabled = var.http_application_routing
+  role_based_access_control {
+    enabled = var.rbac_enabled
+  }
+
+  addon_profile {
+    http_application_routing {
+      enabled = var.http_application_routing
+    }
+
+    oms_agent {
+      enabled                    = var.oms_agent
+      log_analytics_workspace_id = var.log_analytics_workspace_id
+    }
   }
 
   identity {
     type = var.aks_identity_type
-  }
-
-  oms_agent {
-    enabled = var.oms_agent
   }
 
   tags = var.tags
