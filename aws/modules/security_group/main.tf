c65d2dbe6f18c30043a1610fbbe0b3ff918ca5cf
maxo <maxo.webmaster@gmail.com>
2020-12-27 23:28:53 +0100

    broken security group and sns added

--- aws/modules/security_group/main.tf
+++ aws/modules/security_group/main.tf
@@ -4,5 +4,17 @@ resource "aws_security_group" "sgroup" {
   vpc_id                 = var.vpc_id
   revoke_rules_on_delete = var.revoke_rules_on_delete
 
+  dynamic "ingress" {
+    for_each = var.ingress_enabled ? [1] : []
+
+    content {
+      description = var.ingress_description
+      from_port   = var.ingress_from_port
+      to_port     = var.ingress_to_port
+      protocol    = var.ingress_protocol
+      cidr_blocks = var.ingress_cidr_blocks
+    }
+  }
+
   tags = var.tags
 }
