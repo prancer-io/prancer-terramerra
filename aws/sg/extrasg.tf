3d4330f995c0c2a0230d62810659438ad1e18448
harshil <harshil@swansoftweb.com>
2021-09-07 14:19:26 +0550

    Fixed PR-AWS-0176-CFR: AWS Security Groups allow internet traffic to SSH port (22)

--- aws/sg/extrasg.tf
+++ aws/sg/extrasg.tf
@@ -123,17 +123,6 @@ resource "aws_security_group" "default" {
 
   # HTTP access from anywhere
 
-  ingress {
-
-    from_port = 80
-
-    to_port = 80
-
-    protocol = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
 
 
 
