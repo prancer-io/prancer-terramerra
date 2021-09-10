b6053fc852f093b0588033c529cabcb0e0d58836
harshil <harshil@swansoftweb.com>
2021-09-10 15:27:18 +0550

    PR_AWS_0178_TRF: AWS Security Groups with Inbound rule overly permissive to All Traffic

--- aws/modules/security_group_rule/vars.tf
+++ aws/modules/security_group_rule/vars.tf
@@ -1,39 +1,39 @@
 variable "description" {
   description = ""
-  default = ""
+  default     = ""
 }
 
 variable "type" {
   description = "The type of rule being created. Valid options are ingress (inbound) or egress (outbound)."
-  default = "ingress"
+  default     = "ingress"
 }
 
 variable "from_port" {
   description = "The start port."
-  default = 0
+  default     = 0
 }
 
 variable "to_port" {
   description = "The end port."
-  default = 65535
+  default     = 65535
 }
 
 variable "protocol" {
   description = "The protocol. If not icmp, icmpv6, tcp, udp, or all use the protocol number"
-  default = "tcp"
+  default     = "tcp"
 }
 
 variable "cidr_blocks" {
   description = "List of CIDR blocks. Cannot be specified with source_security_group_id."
-  default = []
+  default     = "String<Do not allow all IPv4 traffic from internet>"
 }
 
 variable "self" {
   description = "If true, the security group itself will be added as a source to this ingress rule. "
-  default = null
+  default     = null
 }
 
 variable "security_group_id" {
   description = "The security group to apply this rule to."
-  default = "sg-123456"
+  default     = "sg-123456"
 }
