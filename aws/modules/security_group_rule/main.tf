6ffc31fc9dcedf4467b09fb93f504a58ab2f0592
harshil <harshil@swansoftweb.com>
2021-09-13 13:00:27 +0550

    PR_AWS_0035_TRF: AWS Default Security Group does not restrict all traffic

--- aws/modules/security_group_rule/main.tf
+++ aws/modules/security_group_rule/main.tf
@@ -4,7 +4,7 @@ resource "aws_security_group_rule" "sgroup_rule" {
   from_port         = var.from_port
   to_port           = var.to_port
   protocol          = var.protocol
-  cidr_blocks       = var.cidr_blocks
+  cidr_blocks       = "List<List of IPv4 CIDR blocks, do not allow all traffic by using '0.0.0.0/0'>"
   self              = var.self
   security_group_id = var.security_group_id
 }
