eff71bda9c169966ba5b67bbb2cfec3d75695d99
harshil <harshil@swansoftweb.com>
2021-09-14 11:20:44 +0530

    Added new template with fake access keys

---
+++ aws/secret/main.tf
@@ -0,0 +1,6 @@
+resource "alkira_credential_aws_vpc" "account1" {
+  name           = "customer-aws-1"
+  aws_access_key = "ASIAIOSFODNN7EXAMPLE"
+  aws_secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+  type           = "ACCESS_KEY"
+}
