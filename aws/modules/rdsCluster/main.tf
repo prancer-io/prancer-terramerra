dd5b423734b34352834982156557da7bc92a6a9f
harshil <harshil@swansoftweb.com>
2021-10-01 12:02:31 +0550

    PR-AWS-0262-TRF: Ensure RDS clusters and instances have deletion protection enabled

--- aws/modules/rdsCluster/main.tf
+++ aws/modules/rdsCluster/main.tf
@@ -1,10 +1,11 @@
 resource "aws_rds_cluster" "rdscluster" {
-  cluster_identifier   = var.cluster_identifier
-  engine_mode          = var.cluster_engine_mode
-  master_password      = var.cluster_master_password
-  master_username      = var.cluster_master_username
-  skip_final_snapshot  = var.cluster_skip_final_snapshot
-  kms_key_id           = var.cluster_kms_key_id
-  storage_encrypted    = var.cluster_storage_encrypted
+  cluster_identifier      = var.cluster_identifier
+  engine_mode             = var.cluster_engine_mode
+  master_password         = var.cluster_master_password
+  master_username         = var.cluster_master_username
+  skip_final_snapshot     = var.cluster_skip_final_snapshot
+  kms_key_id              = var.cluster_kms_key_id
+  storage_encrypted       = var.cluster_storage_encrypted
   backup_retention_period = var.cluster_backup_retention_period
+  deletion_protection     = true
 }
