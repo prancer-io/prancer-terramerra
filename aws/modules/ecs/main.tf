0b4c5945bb9c372c009ac58a0546bfcc15563ba7
Harshil <harshil@mail.swansoftweb.net>
2021-11-10 11:38:41 +0550

    Fixed PR-AWS-TRF-ECS-008: Ensure container insights are enabled on ECS cluster

--- aws/modules/ecs/main.tf
+++ aws/modules/ecs/main.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_cluster" "ecs" {
 
   setting {
     name  = "containerInsights"
-    value = var.enable_container_insights ? "enabled" : "disabled"
+    value = "enabled"
   }
 
   tags = var.tags
