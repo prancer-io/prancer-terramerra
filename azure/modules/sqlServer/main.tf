664d1735914ea18475d93807bfa939dea3eff8b7
prancer <prancer@prancer-web-customer115-759b588458-2gtks>
2021-11-14 13:44:15 +0000

    Fixed PR-AZR-TRF-SQL-053: Azure SQL Server audit log retention should be greater then 90 days

--- azure/modules/sqlServer/main.tf
+++ azure/modules/sqlServer/main.tf
@@ -1,11 +1,14 @@
 resource "azurerm_sql_server" "sqlserver" {
-  name                         = var.server_name
-  resource_group_name          = var.server_rg
-  location                     = var.location
+  name                = var.server_name
+  resource_group_name = var.server_rg
+  location            = var.location
 
   version                      = var.server_version
   administrator_login          = var.admin_user
   administrator_login_password = var.admin_password
 
-  tags                         = var.tags
+  tags = var.tags
+  extended_auditing_policy {
+    retention_in_days = 91
+  }
 }
