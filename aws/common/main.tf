7c0c6f35455c279fe525cc3e14f8d7d70f6ed773
prancer <prancer@prancer-web-customer118-794566995b-6sqzt>
2021-11-15 04:57:19 +0000

    Fixed PR-AWS-TRF-AG-002: AWS API gateway request parameter is not validated

--- aws/common/main.tf
+++ aws/common/main.tf
@@ -291,7 +291,7 @@ resource "aws_api_gateway_request_validator" "example" {
   name                        = "example"
   rest_api_id                 = aws_api_gateway_rest_api.example.id
   validate_request_body       = true
-  validate_request_parameters = false
+  validate_request_parameters = true
 }
 
 
