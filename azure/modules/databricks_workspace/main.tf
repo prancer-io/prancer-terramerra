00c5ad12f3ea106b5cd1e5d3cbba752ecb48ea55
r4redu <r4redu@gmail.com>
2021-11-21 09:28:23 -0800

    Fixed PR-AZR-TRF-DBK-002: Azure Databricks should have vnet integration

--- azure/modules/databricks_workspace/main.tf
+++ azure/modules/databricks_workspace/main.tf
@@ -6,6 +6,7 @@ resource "azurerm_databricks_workspace" "adw" {
   tags                = var.tags
 
   custom_parameters {
-    no_public_ip = var.no_public_ip
+    no_public_ip       = var.no_public_ip
+    virtual_network_id = "String<The ID of a Virtual Network where this Databricks Cluster should be created. Changing this forces a new resource to be created.>"
   }
 }
