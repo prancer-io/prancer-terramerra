b1d4705a68a20700db08603a7fb274525c4d3e63
harshil <harshil@mail.swansoftweb.net>
2021-11-26 18:14:52 +0530

    Update resources to make them fail

--- aws/modules/ecs/main.tf
+++ aws/modules/ecs/main.tf
@@ -46,3 +46,12 @@ data "aws_ecs_service" "example" {
   service_name = "example"
   cluster_arn  = data.aws_ecs_cluster.example.arn
 }
+
+
+resource "aws_lb_target_group" "foo" {
+  name        = "tf-example-lb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "instance"
+}
