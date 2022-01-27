8f429a44da1e8a578eac237ae92b068a884a58f0
harshil <harshil@mail.swansoftweb.net>
2022-01-27 14:08:47 +0530

    Added inspector and ecr templates

---
+++ aws/ecr/main.tf
@@ -0,0 +1,19 @@
+resource "aws_ecr_registry_scanning_configuration" "test" {
+  scan_type = "ENHANCED"
+
+  rule {
+    scan_frequency = "SCAN_ON_PUSH"
+    repository_filter {
+      filter      = "*"
+      filter_type = "WILDCARD"
+    }
+  }
+
+  rule {
+    scan_frequency = "CONTINUOUS_SCAN"
+    repository_filter {
+      filter      = "example"
+      filter_type = "WILDCARD"
+    }
+  }
+}
