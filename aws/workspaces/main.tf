4dcffc26c73f4c900ebb5161a1b5b653ac261149
prancer <prancer@prancer-web-customer144-864df6cc4d-2qbb7>
2022-04-22 17:43:27 +0000

    Fixed PR-AWS-TRF-KMS-001: AWS Customer Master Key (CMK) rotation is not enabled

--- aws/workspaces/main.tf
+++ aws/workspaces/main.tf
@@ -118,5 +118,6 @@ resource "aws_directory_service_directory" "example" {
 }
 
 resource "aws_kms_key" "example" {
-  description = "WorkSpaces example key"
+  description         = "WorkSpaces example key"
+  enable_key_rotation = true
 }
