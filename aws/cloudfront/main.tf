78414f1fcab3289416d93e44b3384a75d8867fdc
prancer <prancer@prancer-web-customer2-7598578c97-6269l>
2022-04-27 12:54:04 +0000

    Fixed PR-AWS-TRF-S3-016: Ensure S3 bucket has enabled lock configuration

--- aws/cloudfront/main.tf
+++ aws/cloudfront/main.tf
@@ -1,20 +1,20 @@
 resource "aws_s3_bucket" "s3bucket" {
-    bucket = var.bucket
-    acl = "public-read"
+  bucket = var.bucket
+  acl    = "public-read"
 
-    website {
-        redirect_all_requests_to = "index.html"
-    }
+  website {
+    redirect_all_requests_to = "index.html"
+  }
 
-    cors_rule {
-        allowed_headers = ["*"]
-        allowed_methods = ["*"]
-        allowed_origins = ["*"]
-        expose_headers = ["ETag"]
-        max_age_seconds = 3000
-    }
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["*"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
 
-    policy = <<EOF
+  policy = <<EOF
 {
     "Version": "2008-10-17",
     "Statement": [
@@ -30,10 +30,13 @@ resource "aws_s3_bucket" "s3bucket" {
     ]
 }
 EOF
+  object_lock_configuration {
+    object_lock_enabled = true
+  }
 }
 
 module "cloudfront" {
-  source  = "../modules/cloudfront"
+  source                            = "../modules/cloudfront"
   enabled                           = var.enabled
   is_ipv6_enabled                   = var.is_ipv6_enabled
   comment                           = var.comment
