73e4468ed9498d209d043bbfc55b5880baa332e3
prancer <prancer@prancer-web-customer2-7598578c97-6269l>
2022-04-27 12:32:43 +0000

    Fixed PR-AWS-TRF-S3-019: Ensure S3 bucket RestrictPublicBucket is enabled

--- aws/modules/s3/main.tf
+++ aws/modules/s3/main.tf
@@ -119,6 +119,7 @@ resource "aws_s3_bucket" "s3_bucket" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.s3_bucket.id
 
-  block_public_acls   = false
-  block_public_policy = false
+  block_public_acls       = false
+  block_public_policy     = false
+  restrict_public_buckets = true
 }
