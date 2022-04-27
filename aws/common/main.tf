c541ee9067e109a426e2439874e07d09a3cd494a
prancer <prancer@prancer-web-customer2-7598578c97-6269l>
2022-04-27 12:07:11 +0000

    Fixed PR-AWS-TRF-SGM-002: AWS SageMaker notebook instance with root access enabled

--- aws/common/main.tf
+++ aws/common/main.tf
@@ -576,7 +576,7 @@ resource "aws_sagemaker_notebook_instance" "ni" {
   name                   = "my-notebook-instance"
   role_arn               = aws_iam_role.role.arn
   instance_type          = "ml.t2.medium"
-  root_access            = "Enabled"
+  root_access            = "Disabled"
   direct_internet_access = "Enabled"
 
   subnet_id = []
