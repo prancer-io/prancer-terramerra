84ebe1ff3d9d35f8be8ac185b149313800facd5a
prancer <prancer@prancer-web-customer2-7598578c97-6269l>
2022-04-27 10:31:24 +0000

    Fixed PR-AZR-TRF-SQL-054: Azure MSSQL Server audit log should be enabled

--- azure/modules/mssqlServer/main.tf
+++ azure/modules/mssqlServer/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_mssql_server" "mssqlserver" {
-  name                          = var.server_name
-  resource_group_name           = var.server_rg
-  location                      = var.location
+  name                = var.server_name
+  resource_group_name = var.server_rg
+  location            = var.location
 
   version                       = var.server_version
   administrator_login           = var.admin_user
@@ -13,5 +13,11 @@ resource "azurerm_mssql_server" "mssqlserver" {
     object_id      = var.sql_ad_object_id
   }
 
-  tags                         = var.tags
+  tags = var.tags
+  extended_auditing_policy {
+    storage_endpoint                        = "String<Specifies the blob storage endpoint (e.g. https://MyAccount.blob.core.windows.net). This blob storage will hold all Threat Detection audit logs.>"
+    storage_account_access_key              = "String<Specifies the access key to use for the auditing storage account.>"
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 6
+  }
 }
