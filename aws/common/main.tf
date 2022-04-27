330427bded5a34dce1b190b7964c06f1973447bb
prancer <prancer@prancer-web-customer2-7598578c97-6269l>
2022-04-27 11:54:45 +0000

    Fixed PR-AWS-TRF-GLUE-001: Ensure Glue Data Catalog encryption is enabled

--- aws/common/main.tf
+++ aws/common/main.tf
@@ -786,11 +786,11 @@ resource "aws_glue_data_catalog_encryption_settings" "example" {
   data_catalog_encryption_settings {
     connection_password_encryption {
       aws_kms_key_id                       = aws_kms_key.test.arn
-      return_connection_password_encrypted = false
+      return_connection_password_encrypted = true
     }
 
     encryption_at_rest {
-      catalog_encryption_mode = ""
+      catalog_encryption_mode = "SSE-KMS"
       sse_aws_kms_key_id      = aws_kms_key.test.arn
     }
   }
