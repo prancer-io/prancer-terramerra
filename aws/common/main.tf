5bcfc2fae809bf150ebe6c56a81a37e698fc27cc
prancer <prancer@prancer-web-customer2-7598578c97-6269l>
2022-04-27 12:07:26 +0000

    Fixed PR-AWS-TRF-SGM-003: AWS SageMaker notebook instance configured with direct internet access feature

--- aws/common/main.tf
+++ aws/common/main.tf
@@ -577,7 +577,7 @@ resource "aws_sagemaker_notebook_instance" "ni" {
   role_arn               = aws_iam_role.role.arn
   instance_type          = "ml.t2.medium"
   root_access            = "Enabled"
-  direct_internet_access = "Enabled"
+  direct_internet_access = "Disabled"
 
   subnet_id = []
 
