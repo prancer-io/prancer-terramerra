53c7d93c5efa3b7cb583e2af9f8cfe5dba8d1eca
prancer <prancer@prancer-web-customer2-7598578c97-6269l>
2022-04-27 14:18:09 +0000

    Fixed PR-AWS-TRF-DDB-001: Ensure DocumentDB cluster is encrypted at rest

--- aws/common/main.tf
+++ aws/common/main.tf
@@ -689,7 +689,7 @@ resource "aws_docdb_cluster" "docdb" {
   backup_retention_period         = 5
   preferred_backup_window         = "07:00-09:00"
   skip_final_snapshot             = true
-  storage_encrypted               = false
+  storage_encrypted               = true
   enabled_cloudwatch_logs_exports = false
 }
 
