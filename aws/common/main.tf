d8625f13536992fd5ada0b2bfddf4255a832eac6
prancer <prancer@prancer-web-customer2-7598578c97-6269l>
2022-04-27 13:00:41 +0000

    Fixed PR-AWS-TRF-ECR-001: Ensure ECR image tags are immutable

--- aws/common/main.tf
+++ aws/common/main.tf
@@ -297,7 +297,7 @@ resource "aws_api_gateway_request_validator" "example" {
 
 resource "aws_ecr_repository" "foo" {
   name                 = "bar"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
