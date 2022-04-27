64fd62db5ac834f2ab988b1991256c493f90119f
prancer <prancer@prancer-web-customer2-7598578c97-6269l>
2022-04-27 12:54:17 +0000

    Fixed PR-AWS-TRF-S3-016: Ensure S3 bucket has enabled lock configuration

--- aws/cloudtrail/main.tf
+++ aws/cloudtrail/main.tf
@@ -34,10 +34,13 @@ resource "aws_s3_bucket" "s3" {
     ]
 }
 POLICY
+  object_lock_configuration {
+    object_lock_enabled = true
+  }
 }
 
 module "cloudtrail" {
-  source  = "../modules/cloudtrail"
+  source                        = "../modules/cloudtrail"
   name                          = var.name
   enable_logging                = var.enable_logging
   s3_bucket_name                = aws_s3_bucket.s3.id
