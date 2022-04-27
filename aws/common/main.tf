727a24020a2b0e1a0013a4b348e66e3a7faf39bc
prancer <prancer@prancer-web-customer2-7598578c97-6269l>
2022-04-27 11:55:03 +0000

    Fixed PR-AWS-TRF-GLUE-002: Ensure AWS Glue security configuration encryption is enabled

--- aws/common/main.tf
+++ aws/common/main.tf
@@ -801,16 +801,16 @@ resource "aws_glue_security_configuration" "example" {
 
   encryption_configuration {
     cloudwatch_encryption {
-      cloudwatch_encryption_mode = "DISABLED"
+      cloudwatch_encryption_mode = "SSE-KMS"
     }
 
     job_bookmarks_encryption {
-      job_bookmarks_encryption_mode = "DISABLED"
+      job_bookmarks_encryption_mode = "SSE-KMS"
     }
 
     s3_encryption {
       kms_key_arn        = data.aws_kms_key.example.arn
-      s3_encryption_mode = "DISABLED"
+      s3_encryption_mode = "SSE-KMS"
     }
   }
 }
