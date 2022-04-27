3258b514fe0511ecb8b0da6b06ef2aaf90455a2f
prancer <prancer@prancer-web-customer2-7598578c97-6269l>
2022-04-27 10:37:46 +0000

    Fixed PR-AZR-TRF-SQL-016: Ensure MySQL Database Server accepts only SSL connections

--- azure/modules/mysqlServer/main.tf
+++ azure/modules/mysqlServer/main.tf
@@ -1,17 +1,17 @@
 resource "azurerm_mysql_server" "mysqlserver" {
-  name                          = var.server_name
-  resource_group_name           = var.server_rg
-  location                      = var.location
+  name                = var.server_name
+  resource_group_name = var.server_rg
+  location            = var.location
 
-  administrator_login           = var.admin_user
-  administrator_login_password  = var.admin_password
+  administrator_login          = var.admin_user
+  administrator_login_password = var.admin_password
 
   sku_name   = "GP_Gen5_8"
   storage_mb = 5120
   version    = var.server_version
 
   public_network_access_enabled = var.public_network_access_enabled
-  ssl_enforcement_enabled       = false
+  ssl_enforcement_enabled       = true
 
-  tags                         = var.tags
+  tags = var.tags
 }
