87da09b2df9fd0d4e37d88f51526a908df476c0a
prancer <prancer@prancer-web-customer102-747cf5b6fd-m55z9>
2022-06-14 08:06:15 +0000

    Fixed PR-AZR-TRF-KV-001: Ensure at least one principal has access to Keyvault

--- azure/vault/main.tf
+++ azure/vault/main.tf
@@ -14,8 +14,12 @@ resource "azurerm_key_vault" "example" {
   soft_delete_retention_days = 7
 
   access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+    tenant_id               = data.azurerm_client_config.current.tenant_id
+    object_id               = data.azurerm_client_config.current.object_id
+    storage_permissions     = "List<List of storage permissions, must be one or more from the following: Backup, Delete, DeleteSAS, Get, GetSAS, List, ListSAS, Purge, Recover, RegenerateKey, Restore, Set, SetSAS and Update.>"
+    key_permissions         = "List<List of key permissions, must be one or more from the following: Backup, Create, Decrypt, Delete, Encrypt, Get, Import, List, Purge, Recover, Restore, Sign, UnwrapKey, Update, Verify and WrapKey.>"
+    secret_permissions      = "List< List of secret permissions, must be one or more from the following: Backup, Delete, Get, List, Purge, Recover, Restore and Set.>"
+    certificate_permissions = "List<List of certificate permissions, must be one or more from the following: Backup, Create, Delete, DeleteIssuers, Get, GetIssuers, Import, List, ListIssuers, ManageContacts, ManageIssuers, Purge, Recover, Restore, SetIssuers and Update.>"
   }
 }
 
