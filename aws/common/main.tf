3615db4196164116383edf444e5be2d67a07a9f7
jaimin <jaimin@swansoftweb.com>
2022-07-14 14:03:49 +0530

    added aws_elasticache_cluster resource

--- aws/common/main.tf
+++ aws/common/main.tf
@@ -1230,3 +1230,13 @@ resource "aws_secretsmanager_secret_rotation" "secret-manager-example" {
     automatically_after_days = 35
   }
 }
+
+resource "aws_elasticache_cluster" "example" {
+  cluster_id           = "cluster-example"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
+}
