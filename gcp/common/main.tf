180ce4daeb0b013fbffa68f3b941eb3300f7f4bc
ishan <Ishan.Pansuriya@prancer.io>
2022-10-21 14:55:44 +0530

    added new taraform data

--- gcp/common/main.tf
+++ gcp/common/main.tf
@@ -1,3 +1,4 @@
+# Bucket Iam
 resource "google_storage_bucket_iam_policy" "policy" {
     bucket = google_storage_bucket.default.name
     role = "roles/storage.admin"
@@ -6,7 +7,74 @@ resource "google_storage_bucket_iam_policy" "policy" {
     ]
 }
 
+# Project metadata 
 resource "google_compute_project_metadata_item" "default" {
-  key   = "my_metadata"
-  value = "my_value"
+  key   = "enable-oslogin"
+  value = "false"
+}
+
+# Project IAM 
+resource "google_project_iam_policy" "project" {
+  project     = "your-project-id"
+  policy_data = "${data.google_iam_policy.admin.policy_data}"
+}
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/compute.admin"
+
+    members = [
+      "user:jane@example.com",
+    ]
+
+    condition {
+      title       = "expires_after_2019_12_31"
+      description = "Expiring at midnight of 2019-12-31"
+      expression  = "request.time < timestamp(\"2020-01-01T00:00:00Z\")"
+    }
+  }
+}
+
+# Project Iam (Only binding)
+resource "google_project_iam_binding" "project" {
+  project = "your-project-id"
+  role    = "roles/container.admin"
+
+  members = [
+    "user:jane@example.com",
+  ]
+
+  condition {
+    title       = "expires_after_2019_12_31"
+    description = "Expiring at midnight of 2019-12-31"
+    expression  = "request.time < timestamp(\"2020-01-01T00:00:00Z\")"
+  }
+}
+
+# Project Iam (Only Member)
+resource "google_project_iam_member" "project" {
+  project = "your-project-id"
+  role    = "roles/firebase.admin"
+  member  = "user:jane@example.com"
+
+  condition {
+    title       = "expires_after_2019_12_31"
+    description = "Expiring at midnight of 2019-12-31"
+    expression  = "request.time < timestamp(\"2020-01-01T00:00:00Z\")"
+  }
+}
+
+# Project Iam Audit
+resource "google_project_iam_audit_config" "project" {
+  project = "your-project-id"
+  service = "allServices"
+  audit_log_config {
+    log_type = "ADMIN_READ"
+  }
+  audit_log_config {
+    log_type = "DATA_READ"
+    exempted_members = [
+      "user:joebloggs@hashicorp.com",
+    ]
+  }
 }
