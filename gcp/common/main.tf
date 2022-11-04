7587b04729535c2ac16dbc102402a56c6226bc73
ishan <Ishan.Pansuriya@prancer.io>
2022-11-04 16:41:28 +0530

    security policy resource  added

--- gcp/common/main.tf
+++ gcp/common/main.tf
@@ -88,3 +88,36 @@ resource "google_app_engine_application" "app" {
   serving_status = "SERVING"
   iap = "iap"
 }
+
+# security policy
+
+resource "google_compute_security_policy" "policy" {
+  name = "my-policy"
+
+  rule {
+    action   = "deny(403)"
+    priority = "1000"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["9.9.9.0/24"]
+      }
+    }
+    description = "Deny access to IPs in 9.9.9.0/24"
+  }
+
+  rule {
+    action   = "allow"
+    priority = "2147483647"
+    match {
+      expr {
+        expression = "cve-canary
+      }
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+    description = "default rule"
+  }
+}
