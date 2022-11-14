86f5dd8c28a2778da2aaba87e095dbc94e2b262d
prancer <prancer@prancer-web-customer148-5cb6475774-lmpmz>
2022-11-14 11:08:04 +0000

    Fixed PR-AWS-TRF-S3-001: AWS Access logging not enabled on S3 buckets

--- aws/msk/main.tf
+++ aws/msk/main.tf
@@ -39,6 +39,11 @@ resource "aws_cloudwatch_log_group" "test" {
 resource "aws_s3_bucket" "bucket" {
   bucket = "msk-broker-logs-bucket"
   acl    = "private"
+
+  logging {
+    target_prefix = "String< To specify a key prefix for log objects>"
+    target_bucket = "String<The name of the bucket that will receive the log objects>"
+  }
 }
 
 resource "aws_iam_role" "firehose_role" {
