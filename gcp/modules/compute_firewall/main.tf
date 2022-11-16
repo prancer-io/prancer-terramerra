71759a4636408c38bd67d9bfbac9f8c035e0a77a
ishan <Ishan.Pansuriya@prancer.io>
2022-11-16 17:44:21 +0530

    modified firewall trf

--- gcp/modules/compute_firewall/main.tf
+++ gcp/modules/compute_firewall/main.tf
@@ -4,9 +4,8 @@ resource "google_compute_firewall" "firewall" {
   network   = var.fw_network_id
   direction = var.fw_direction
 
-  allow {
-    fw_allows = var.fw_allows 
-  }
+  allow = var.fw_allows 
+
 
   source_ranges = var.fw_source_ranges 
   source_tags   = var.fw_source_tags
