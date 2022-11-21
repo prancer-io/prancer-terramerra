6f001f9420933f3fd60210b64151070efabdb0dc
ishan <Ishan.Pansuriya@prancer.io>
2022-11-21 17:15:16 +0530

    modified terraform

--- gcp/modules/compute_firewall/main.tf
+++ gcp/modules/compute_firewall/main.tf
@@ -1,13 +1,11 @@
 
+
 resource "google_compute_firewall" "firewall" {
   name      = var.fw_name
   network   = var.fw_network_id
   direction = var.fw_direction
 
-  allow = [{
-  protocol = "udp"
-  ports    = ["20-28000"]
-  }]
+  allow = [var.fw_allows]
 
   source_ranges = var.fw_source_ranges
   source_tags   = var.fw_source_tags
