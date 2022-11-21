e81296ebbf673ec45870b4d0c43d616cee593586
ishan <Ishan.Pansuriya@prancer.io>
2022-11-21 17:40:30 +0530

    moified firewall gcp

--- gcp/modules/compute_firewall/main.tf
+++ gcp/modules/compute_firewall/main.tf
@@ -5,10 +5,10 @@ resource "google_compute_firewall" "firewall" {
   network   = var.fw_network_id
   direction = var.fw_direction
 
-  allow = {
+  allow = [{
   protocol = "udp"
   ports    = ["20-28000"]
-  }
+  }]
 
   source_ranges = var.fw_source_ranges
   source_tags   = var.fw_source_tags
