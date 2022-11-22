4862e7e17db403e808f0e2fef48f197fda4ccc1b
ishan <Ishan.Pansuriya@prancer.io>
2022-11-22 10:04:35 +0550

    Fixed PR-GCP-TRF-FW-002: GCP Firewall rule allows internet traffic to DNS port (53)

--- gcp/modules/compute_firewall/main.tf
+++ gcp/modules/compute_firewall/main.tf
@@ -5,10 +5,7 @@ resource "google_compute_firewall" "firewall" {
   network   = var.fw_network_id
   direction = var.fw_direction
 
-  allow = [{
-  protocol = "udp"
-  ports    = ["20-28000"]
-  }]
+  allow = []
 
   source_ranges = var.fw_source_ranges
   source_tags   = var.fw_source_tags
