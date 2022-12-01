90e709281d2710fa5b66c11eee33ce69ab8a1764
ishan <Ishan.Pansuriya@prancer.io>
2022-12-01 15:26:17 +0550

    Fixed PR-GCP-TRF-INST-013: Ensure GCP VM instance with Shielded VM features enabled

--- gcp/modules/compute_instance/vars.tf
+++ gcp/modules/compute_instance/vars.tf
@@ -1,76 +1,76 @@
 variable "vm_name" {
-  type = string
+  type    = string
   default = ""
 }
 variable "machine_type" {
-  type = string
+  type    = string
   default = "f1-micro"
 }
 variable "zone" {
-  type = string
+  type    = string
   default = null
 }
 variable "vm_tags" {
-  type = list
+  type    = list
   default = []
 }
 variable "vm_image" {
-  type = string
+  type    = string
   default = "debian-cloud/debian-9"
 }
 variable "scheduling_enabled" {
-  type = bool
+  type    = bool
   default = false
 }
 variable "scheduling_preemptible" {
-  type = bool
+  type    = bool
   default = false
 }
 variable "scheduling_on_host_maintenance" {
-  type = string
+  type    = string
   default = null
 }
 variable "scheduling_automatic_restart" {
-  type = bool
+  type    = bool
   default = false
 }
 variable "network" {
-  type = string
+  type    = string
   default = "default"
 }
 variable "subnetwork" {
-  type = string
+  type    = string
   default = null
 }
 variable "vm_access_config" {
-  type = map
+  type    = map
   default = {}
 }
 variable "enable_secure_boot" {
-  type = bool
-  default = false
+  type    = bool
+  default = true
 }
 variable "email" {
-  type = string
+  type    = string
   default = ""
 }
 variable "scopes" {
-  type = string
+  type    = string
   default = ""
 }
 variable "can_ip_forward" {
-  type = bool
+  type    = bool
   default = false
 }
 variable "vm_metadata" {
-  type = map
+  type    = map
   default = {}
 }
 variable "metadata_startup_script" {
-  type = string
+  type    = string
   default = ""
 }
 variable "labels" {
-  type = map
+  type    = map
   default = {}
 }
