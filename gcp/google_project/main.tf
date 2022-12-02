ab90b2f3e9932ac30350c6fc5a56476bd004868d
ishan <Ishan.Pansuriya@prancer.io>
2022-12-02 14:09:43 +0530

    modified google_project

--- gcp/google_project/main.tf
+++ gcp/google_project/main.tf
@@ -1,7 +1,7 @@
 resource "google_project" "my_project-in-a-folder" {
-  name       = "My Project"
-  project_id = "your-project-id"
-  folder_id  = google_folder.department1.name
+  name                = "My Project"
+  project_id          = "your-project-id"
+  folder_id           = google_folder.department1.name
   auto_create_network = false
 }
 
