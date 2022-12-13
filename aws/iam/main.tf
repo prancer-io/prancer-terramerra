3b9209bf8a3322faebb9d3af2c5e70b35f578991
jaimin <jaimin@swansoftweb.com>
2022-12-13 16:08:48 +0530

    modify tags in AWS iam templates

--- aws/iam/main.tf
+++ aws/iam/main.tf
@@ -75,6 +75,6 @@ resource "aws_iam_user" "Test" {
 
   tags = {
     prancer_unique_id = "c3b370c9-a596-416b-bf2c-265a6bd1c056"
-    resource_type = "aws::iam::user"
+    resource_type = "aws_iam_user"
   }
 }
