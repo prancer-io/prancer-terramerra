1d046bb3158481117ffbf33ebd5ce77b7570293a
jaimin <jaimin@swansoftweb.com>
2023-01-10 16:33:59 +0530

    added tags in azurerm_virtual_network vnet

--- azure/modules/virtualNetwork/main.tf
+++ azure/modules/virtualNetwork/main.tf
@@ -3,5 +3,8 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = var.vnet_rg
   address_space       = [var.address_space]
   location            = var.location
-  tags                = var.tags
+  tags                = {
+    "prancer_unique_id": "15f36246-6937-45b3-94f1-dfd76cc0bef5",
+    "resource_type": "azurerm_virtual_machine"
+  }
 }
